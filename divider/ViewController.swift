0be2775a4406dc43b542d8282a4ac95679b986a9
Artem Grebenkin <artoisgrebenkin@gmail.com>
2019-07-24 22:41:18 +0600

    initial commit

---
+++ divider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  divider
+//
+//  Created by Artem Grebenkin on 7/24/19.
+//  Copyright © 2019 Artem Grebenkin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+
+}
+
